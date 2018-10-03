b8a88706bad34bd3f36806fe197919cec1fca6ef
Rich <rchrdholme@gmail.com>
2018-10-02 21:12:49 -0400

    images and index setup

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".info").hide();
+	$(".mainProjectInfo").hide();
+
+	$(".infoBtn").on('click', function(){
+		event.preventDefault();
+		let id = $(this).attr("id");
+		$("." + id + "Info").show();
+		$("." + id).hide();
+	})
+
+	$(".close").on('click', function(){
+		event.preventDefault();
+		let id = $(this).attr("id");
+		id = id.substring(0, id.length - 5);
+		$("." + id + "Info").hide();
+		$("." + id).addClass("animated");
+		$("." + id).addClass("fadeInUp");
+		$("." + id).show();
+	})
+})
